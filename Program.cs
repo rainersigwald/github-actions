54b06944cb41e4322f2d37a1ec1be5a643f8d770
Jared Parsons <jaredpparsons@gmail.com>
2020-06-25 09:01:47 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github_actions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
